160f946cd745d8a61cf1d6d33eebe8648ea0bf71
suzukiplan <suzukiplan.ys@gmail.com>
2020-05-13 00:02:09 +0900

    refactor & small bugfix

--- src/ppu.hpp
+++ src/ppu.hpp
@@ -82,6 +82,28 @@ class PPU
     {
         fprintf(stdout, "PPU read from $%04X (PPU clock: %dHz - line:%d)\n", addr, R.clock, R.line); // 暫定処理（後で消す）
         switch (addr) {
+            /**
+             * 7  bit  0
+             * ---- ----
+             * VSO. ....
+             * |||| ||||
+             * |||+-++++- Least significant bits previously written into a PPU register
+             * |||        (due to register not being updated for this address)
+             * ||+------- Sprite overflow. The intent was for this flag to be set
+             * ||         whenever more than eight sprites appear on a scanline, but a
+             * ||         hardware bug causes the actual behavior to be more complicated
+             * ||         and generate false positives as well as false negatives; see
+             * ||         PPU sprite evaluation. This flag is set during sprite
+             * ||         evaluation and cleared at dot 1 (the second dot) of the
+             * ||         pre-render line.
+             * |+-------- Sprite 0 Hit.  Set when a nonzero pixel of sprite 0 overlaps
+             * |          a nonzero background pixel; cleared at dot 1 of the pre-render
+             * |          line.  Used for raster timing.
+             * +--------- Vertical blank has started (0: not in vblank; 1: in vblank).
+             *            Set at dot 1 of line 241 (the line *after* the post-render
+             *            line); cleared after reading $2002 and at dot 1 of the
+             *            pre-ren der line.
+             */
             case 0x2002: {
                 R.internalFlag = 0;
                 unsigned char result = R.status;
@@ -102,12 +124,6 @@ class PPU
                 R.ctrl = value;
                 _updateWorkAreaCtrl();
                 break;
-            case 0x2003:
-                R.oamAddr = value;
-                break;
-            case 0x2004:
-                M.oam[R.oamAddr] = value;
-                break;
             case 0x2001: // Mask
                 /**
                  * BGRs bMmG
@@ -122,6 +138,12 @@ class PPU
                  * +--------- Emphasize blue
                  */
                 R.mask = value;
+                break;
+            case 0x2003:
+                R.oamAddr = value;
+                break;
+            case 0x2004:
+                M.oam[R.oamAddr] = value;
                 break;
             case 0x2005: // Scroll
                 R.scroll[R.internalFlag & 1] = value;
@@ -184,7 +206,7 @@ class PPU
             if (W.ctrl.generateNMI) cpu->NMI();
             CB.endOfFrame(CB.arg);
         } else if (R.line == 261 && pixel == 1) {
-            R.status &= 0b01111111;
+            R.status &= 0b00011111;
         }
     }
 
